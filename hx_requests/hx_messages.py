56a5604ca5f35d8459717970096193e321bfab84
yaakovLowenstein <lowensteinyaakov@gmail.com>
2023-04-02 08:28:07 -0400

    fix: 🐛 in trying to use dynamic functions for types of messages

--- hx_requests/hx_messages.py
+++ hx_requests/hx_messages.py
@@ -1,4 +1,4 @@
-from typing import List, Tuple
+from typing import Dict, List, Tuple
 
 from django.conf import settings
 
@@ -13,43 +13,39 @@ class HXMessageTags:
 
 class HXMessages:
     messages: List[Tuple[str, str]]
+    tags: Dict[int, str]
     # TODO USE json script tag (and use include template for this) and would also need context processr
-
-    tags = ["debug", "info", "success", "warning", "error"]
 
     def init__(self) -> None:
         self.messages = []
         self.settings_dict = getattr(settings, "HX_REQUESTS_HX_MESSAGES", {})
-        tags_mapping = self.get_tags_mapping()
+        self.set_tags()
 
-        for tag in self.tags:
-            self.tag = lambda message: self.messages.append(
-                (message, tags_mapping.get(tag))
-            )
+    def debug(self, message):
+        self.messages.append((message, self.tags.get(10)))
 
-    def get_tags_mapping(self):
+    def info(self, message):
+        self.messages.append((message, self.tags.get(20)))
+
+    def success(self, message):
+        self.messages.append((message, self.tags.get(25)))
+
+    def warning(self, message):
+        self.messages.append((message, self.tags.get(30)))
+
+    def error(self, message):
+        self.messages.append((message, self.tags.get(40)))
+
+    def set_tags(self):
         if self.settings_dict.get("USE_DJANGO_MESSAGE_TAGS"):
-            tags_mapping = getattr(settings, "MESSAGE_TAGS")
+            self.tags = getattr(settings, "MESSAGE_TAGS")
         else:
-            tags_mapping = self.settings_dict.get("HX_MESSAGE_TAGS")
+            self.tags = self.settings_dict.get("HX_MESSAGE_TAGS")
 
-        if not tags_mapping:
+        if not self.tags:
             raise Exception(
                 "HX_MESSAGE_TAGS must be defined in settings to use messages with hx-requests."
             )
-
-        new_mapping = {}
-        for key, value in tags_mapping:
-            if key == 10:
-                new_mapping["debug"] = value
-            elif key == 20:
-                new_mapping["info"] = value
-            elif key == 25:
-                new_mapping["success"] = value
-            elif key == 30:
-                new_mapping["warning"] = value
-            elif key == 40:
-                new_mapping["error"] = value
 
     def get_message(self):
         if self.messages:
