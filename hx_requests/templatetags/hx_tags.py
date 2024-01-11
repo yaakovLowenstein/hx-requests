bf720ac0d4593c207500d57e1ffd117941f2187c
Yaakov Lowenstein <lowensteinyaakov@gmail.com>
2024-01-11 15:53:50 -0500

    feat: Add ability for hx url to use the full path (Includes query params)

--- hx_requests/templatetags/hx_tags.py
+++ hx_requests/templatetags/hx_tags.py
@@ -9,13 +9,13 @@ register = template.Library()
 
 @register.simple_tag(takes_context=True)
 def render_hx(
-    context: Dict, hx_request_name: str, method: str = "get", object=None, **kwargs
+    context: Dict, hx_request_name: str, method: str = "get", object=None, use_full_path=False, **kwargs
 ) -> str:
     """
     Renders out the hx-post or hx-get with the correct url along with all kwargs
     turned into GET params.
     """
-    url = get_url(context, hx_request_name, object, **kwargs)
+    url = get_url(context, hx_request_name, object,use_full_path ,**kwargs)
 
     token = get_csrf_token(context)
     return f""" hx-{method}={url}
@@ -24,16 +24,16 @@ def render_hx(
 
 
 @register.simple_tag(takes_context=True)
-def hx_get(context: Dict, hx_request_name: str, object=None, **kwargs) -> str:
+def hx_get(context: Dict, hx_request_name: str, object=None, use_full_path=False, **kwargs) -> str:
     """
     Same as render_hx EXCEPT that it is specifically for 'get' requests.
     """
-    return render_hx(context, hx_request_name, method="get", object=object, **kwargs)
+    return render_hx(context, hx_request_name, method="get", object=object, use_full_path=use_full_path, **kwargs)
 
 
 @register.simple_tag(takes_context=True)
-def hx_post(context: Dict, hx_request_name: str, object=None, **kwargs) -> str:
+def hx_post(context: Dict, hx_request_name: str, object=None,use_full_path=False ,**kwargs) -> str:
     """
     Same as render_hx EXCEPT that it is specifically for 'post' requests.
     """
-    return render_hx(context, hx_request_name, method="post", object=object, **kwargs)
+    return render_hx(context, hx_request_name, method="post", object=object, use_full_path=use_full_path, **kwargs)
