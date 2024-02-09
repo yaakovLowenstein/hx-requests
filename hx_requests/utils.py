a429e89eed2897bc97a7b3515d16083b9b2d3466
Yaakov Lowenstein <lowensteinyaakov@gmail.com>
2024-02-09 11:06:33 -0500

    fix: Fix bugs in serialization and with use_full_path
    1. Added prefix to kwargs to avoid conflicts with other query parameters
       i.e if there are GET params with the same name we dont want them
       to conflict (like page filters)
    2. Fixed the use_full_path logic to correctly add the GET params
       to the url by using the getlist method to get all values for a key.

--- hx_requests/utils.py
+++ hx_requests/utils.py
@@ -1,56 +1,52 @@
+[11:02 AM] Yael Lundner
 from urllib.parse import quote_plus
-
 from django.apps import apps
 from django.db import models
 from urllib.parse import urlencode, quote_plus
-
-
+KWARG_PREFIX = "___"
 def is_htmx_request(request):
     return "HX-Request" in request.headers
 
-
 def serialize_kwargs(**kwargs):
+    serialize_kwargs = {}
     for k, v in kwargs.items():
+        prefixed_k = KWARG_PREFIX + k
         if isinstance(v, models.Model):
-            kwargs[k] = f"___{v._meta.app_label}_{v._meta.model.__name__}_{v.pk}"
-    return kwargs
-
+            serialize_kwargs[prefixed_k] = f"{KWARG_PREFIX}{v._meta.app_label}_{v._meta.model.__name__}_{v.pk}"
+        else:
+            serialize_kwargs[prefixed_k] = v
+    return serialize_kwargs
 
 def deserialize_kwargs(**kwargs):
+    deserialize_kwargs = {}
     for k, v in kwargs.items():
-        if v.startswith("___"):
-            app_label, model, pk = v.replace("___", "").split("_")
+        prefixed_k = k.replace(KWARG_PREFIX, "")
+        if v.startswith(KWARG_PREFIX):
+            app_label, model, pk = v.replace(KWARG_PREFIX, "").split("_")
             model = apps.get_model(app_label, model)
             instance = model.objects.get(pk=pk)
-            kwargs[k] = instance
-    return kwargs
-
+            deserialize_kwargs[prefixed_k] = instance
+        else:
+            deserialize_kwargs[prefixed_k] = v
+    return deserialize_kwargs
 
 def get_url(context, hx_request_name, obj, use_full_path=False, **kwargs):
     request = context["request"]
     url = request.path
-
     params = {"hx_request_name": hx_request_name}
-
     if use_full_path:
-        params.update(
-            {
-                k: v
-                for k, v in request.GET.items()
-                if k not in ["hx_request_name", "object"]
-            }
-        )
+        get_params ={}
+        for k in request.GET.keys():
+            if k not in ["hx_request_name", "object"]:
+                get_params[k] = request.GET.getlist(k)
+        params.update(get_params)
 
     if obj:
         params["object"] = f"{obj._meta.app_label}_{obj._meta.model.__name__}_{obj.pk}"
-
     url += f"?{urlencode(params)}"
-
     serialized_kwargs = serialize_kwargs(**kwargs)
     url += "".join(f"&{k}={quote_plus(str(v))}" for k, v in serialized_kwargs.items())
-
     return url
-
 
 def get_csrf_token(context):
     cookie = context["request"].headers.get("cookie")
