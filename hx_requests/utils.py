beebca1df69dadfd08366b0a4cd5b5b3a9b85a88
Yaakov Lowenstein <lowensteinyaakov@gmail.com>
2024-02-12 15:34:40 -0500

    fix: Fix bug with url kwargs
    It was turning all the kwargs into lists.

--- hx_requests/utils.py
+++ hx_requests/utils.py
@@ -1,11 +1,13 @@
-[11:02 AM] Yael Lundner
 from urllib.parse import quote_plus
+
 from django.apps import apps
 from django.db import models
 from urllib.parse import urlencode, quote_plus
+
 KWARG_PREFIX = "___"
 def is_htmx_request(request):
     return "HX-Request" in request.headers
+
 
 def serialize_kwargs(**kwargs):
     serialize_kwargs = {}
@@ -16,6 +18,7 @@ def serialize_kwargs(**kwargs):
         else:
             serialize_kwargs[prefixed_k] = v
     return serialize_kwargs
+
 
 def deserialize_kwargs(**kwargs):
     deserialize_kwargs = {}
@@ -30,23 +33,31 @@ def deserialize_kwargs(**kwargs):
             deserialize_kwargs[prefixed_k] = v
     return deserialize_kwargs
 
+
 def get_url(context, hx_request_name, obj, use_full_path=False, **kwargs):
     request = context["request"]
     url = request.path
+
     params = {"hx_request_name": hx_request_name}
+
     if use_full_path:
         get_params ={}
-        for k in request.GET.keys():
+        for k, v in request.GET.lists():
             if k not in ["hx_request_name", "object"]:
-                get_params[k] = request.GET.getlist(k)
+                get_params[k] = v[0] if len(v) == 1 else v
         params.update(get_params)
+
 
     if obj:
         params["object"] = f"{obj._meta.app_label}_{obj._meta.model.__name__}_{obj.pk}"
+
     url += f"?{urlencode(params)}"
+
     serialized_kwargs = serialize_kwargs(**kwargs)
     url += "".join(f"&{k}={quote_plus(str(v))}" for k, v in serialized_kwargs.items())
+
     return url
+
 
 def get_csrf_token(context):
     cookie = context["request"].headers.get("cookie")
