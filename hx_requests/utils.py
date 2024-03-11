e63523efa4db25f4c9593caf5a620ebaa4badbee
yaakovLowenstein <lowensteinyaakov@gmail.com>
2024-03-11 00:07:28 +0000

    fix: Fix the way serialization is being done
    There was a bug since was serializing with one '_' it would break
    if the app name had an underscore. Now it uses two underscores.
    
    Additionally, now using json dumps and loads so that lists and
    booleans are properly serialized and deserialized.

--- hx_requests/utils.py
+++ hx_requests/utils.py
@@ -1,37 +1,49 @@
-from urllib.parse import quote_plus, urlparse
+import json
+from urllib.parse import quote_plus, urlencode, urlparse
 
 from django.apps import apps
 from django.db import models
-from urllib.parse import urlencode, quote_plus
 
+MODEL_INSTANCE_PREFIX = "model_instance__"
 KWARG_PREFIX = "___"
+__ = "__"
+
+
+def serialize(value):
+    if isinstance(value, models.Model):
+        return f"{MODEL_INSTANCE_PREFIX}{value._meta.app_label}{__}{value._meta.model_name}{__}{value.pk}"
+    return json.dumps(value)
+
+
+def deserialize(value):
+    if value.startswith(MODEL_INSTANCE_PREFIX):
+        value = value.replace(MODEL_INSTANCE_PREFIX, "")
+        app_label, model_name, pk = value.split(__)
+        model = apps.get_model(app_label, model_name)
+        return model.objects.get(pk=pk)
+    return json.loads(value)
+
+
 def is_htmx_request(request):
     return "HX-Request" in request.headers
 
 
 def serialize_kwargs(**kwargs):
-    serialize_kwargs = {}
+    serialized_kwargs = {}
     for k, v in kwargs.items():
         prefixed_k = KWARG_PREFIX + k
-        if isinstance(v, models.Model):
-            serialize_kwargs[prefixed_k] = f"{KWARG_PREFIX}{v._meta.app_label}_{v._meta.model.__name__}_{v.pk}"
-        else:
-            serialize_kwargs[prefixed_k] = v
-    return serialize_kwargs
+        serialized_kwargs[prefixed_k] = serialize(v)
+    return serialized_kwargs
 
 
 def deserialize_kwargs(**kwargs):
-    deserialize_kwargs = {}
+    deserialized_kwargs = {}
     for k, v in kwargs.items():
+        if not k.startswith(KWARG_PREFIX):
+            continue
         prefixed_k = k.replace(KWARG_PREFIX, "")
-        if v.startswith(KWARG_PREFIX):
-            app_label, model, pk = v.replace(KWARG_PREFIX, "").split("_")
-            model = apps.get_model(app_label, model)
-            instance = model.objects.get(pk=pk)
-            deserialize_kwargs[prefixed_k] = instance
-        else:
-            deserialize_kwargs[prefixed_k] = v
-    return deserialize_kwargs
+        deserialized_kwargs[prefixed_k] = deserialize(v)
+    return deserialized_kwargs
 
 
 def get_url(context, hx_request_name, obj, use_full_path=False, **kwargs):
@@ -43,12 +55,14 @@ def get_url(context, hx_request_name, obj, use_full_path=False, **kwargs):
     if use_full_path:
         get_params = {}
         for k, v in request.GET.lists():
-            if k not in ["hx_request_name", "object"] and not k.startswith(KWARG_PREFIX):
+            if k not in ["hx_request_name", "object"] and not k.startswith(
+                KWARG_PREFIX
+            ):
                 get_params[k] = v[0] if len(v) == 1 else v
         params.update(get_params)
 
     if obj:
-        params["object"] = f"{obj._meta.app_label}_{obj._meta.model.__name__}_{obj.pk}"
+        params["object"] = serialize(obj)
 
     # Parse the URL to check if there are existing query parameters
     parsed_url = urlparse(url)
@@ -63,6 +77,7 @@ def get_url(context, hx_request_name, obj, use_full_path=False, **kwargs):
     url += "".join(f"&{k}={quote_plus(str(v))}" for k, v in serialized_kwargs.items())
 
     return url
+
 
 def get_csrf_token(context):
     cookie = context["request"].headers.get("cookie")
