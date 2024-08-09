ea2ee742575534fce71e033e3e489b6608bf95b4
Yaakov Lowenstein <lowensteinyaakov@gmail.com>
2024-08-09 21:43:15 +0000

    feat: Change the way HXRequests are registered
    Using singleton registry class that is thread-safe and lazy-loaded.
    At a high level these are why using class attr is a bad idea:
    
    - Thread Safety: Potential for race conditions and data corruption.
    - Global State Management: Difficulties in controlling and managing global state.
    - Initialization Control: Redundant and potentially inconsistent initialization logic.
    - Modularity and Extensibility: Tighter coupling and reduced flexibility.
    - Consistency Across Instances: Challenges in maintaining consistent state across multiple instances.

---
+++ hx_requests/hx_registry.py
@@ -0,0 +1,59 @@
+import importlib
+import inspect
+import threading
+
+from django.apps import apps
+
+from hx_requests.hx_requests import BaseHXRequest
+
+
+class HXRequestRegistry:
+    _registry = {}
+    _initialized = False
+    _lock = threading.Lock()  # Lock to ensure thread safety
+    _processed_classes = set()  # Keep track of processed classes
+
+    @classmethod
+    def initialize(cls):
+        if cls._initialized:
+            return
+
+        with cls._lock:  # Ensure that only one thread can initialize at a time
+            if cls._initialized:
+                return  # Another thread might have finished initializing while we were waiting
+
+            # Proceed with initialization
+            for app in apps.get_app_configs():
+                try:
+                    module = importlib.import_module(f"{app.label}.hx_requests")
+                    clsmembers = inspect.getmembers(module, inspect.isclass)
+                    for _, obj in clsmembers:
+                        # Check if the class has already been processed
+                        if obj in cls._processed_classes:
+                            continue
+                        cls._processed_classes.add(obj)
+
+                        if issubclass(obj, BaseHXRequest) and getattr(
+                            obj, "name", None
+                        ):
+                            cls.register_hx_request(obj.name, obj)
+                except ModuleNotFoundError:
+                    continue
+
+            cls._initialized = True
+
+    @classmethod
+    def register_hx_request(cls, name, hx_request_class):
+        if name in cls._registry:
+            raise Exception(f"Duplicate HXRequest name found: {name}")
+        cls._registry[name] = hx_request_class
+
+    @classmethod
+    def get_hx_request(cls, name):
+        cls.initialize()
+        return cls._registry.get(name)
+
+    @classmethod
+    def get_all_hx_requests(cls):
+        cls.initialize()
+        return cls._registry
--- hx_requests/views.py
+++ hx_requests/views.py
@@ -1,5 +1,3 @@
-import importlib
-import inspect
 from typing import Dict
 
 from django.apps import apps
@@ -8,6 +6,7 @@ from django.http import Http404
 from django.utils.decorators import method_decorator
 from django.views.decorators.csrf import ensure_csrf_cookie
 
+from hx_requests.hx_registry import HXRequestRegistry
 from hx_requests.utils import deserialize_kwargs, is_htmx_request
 
 
@@ -41,48 +40,12 @@ class HtmxViewMixin:
 
     def get_hx_request(self, request):
         hx_request_name = request.GET.get("hx_request_name")
-        self._get_hx_request_classes()
-        try:
-            hx_request = next(
-                hx_request
-                for name, hx_request in self.hx_requests.items()
-                if name == hx_request_name
-            )
-        except StopIteration:
-            raise Exception(
+        hx_request_class = HXRequestRegistry.get_hx_request(hx_request_name)
+        if not hx_request_class:
+            raise Http404(
                 f"No HXRequest found with the name {hx_request_name}. Are you sure it's spelled correctly?"
             )
-        return hx_request()
-
-    @classmethod
-    def _get_hx_request_classes(cls):
-        from .hx_requests import BaseHXRequest
-
-        # If the hx_requests are already set, don't need to do the whole collection.
-        if getattr(cls, "hx_requests", None):
-            return
-
-        processed_classes = set()  # Keep track of processed classes
-        hx_request_classes = {}
-
-        for app in apps.get_app_configs():
-            try:
-                module = importlib.import_module(f"{app.label}.hx_requests")
-                clsmembers = inspect.getmembers(module, inspect.isclass)
-                for _, obj in clsmembers:
-                    # Check if the class has already been processed
-                    if obj in processed_classes:
-                        continue
-                    processed_classes.add(obj)
-
-                    if issubclass(obj, BaseHXRequest) and getattr(obj, "name", None):
-                        if obj.name in hx_request_classes:
-                            raise Exception(
-                                f"Duplicate HXRequest name found: {obj.name}. Please ensure all HXRequests have unique names."
-                            )
-                        hx_request_classes[obj.name] = obj
-            except ModuleNotFoundError:
-                pass
+        return hx_request_class()
 
     def get_hx_extra_kwargs(self, request):
         kwargs = {}
