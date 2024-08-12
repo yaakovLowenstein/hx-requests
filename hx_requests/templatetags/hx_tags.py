0018ca563170d9cd9100b80ce9c26c139d856afe
Yaakov Lowenstein <lowensteinyaakov@gmail.com>
2024-08-12 01:03:36 +0000

    feat! Remove render_hx template tag
    BREAKING CHANGE: The render_hx template tag has been removed.
    It has been replaced by the hx_get and hx_post template tags.
    The hx_get and hx_post template tags are more explicit
    and easier to understand. The render_hx template tag was
    confusing and not as clear as the hx_get and hx_post template tags.

--- hx_requests/templatetags/hx_tags.py
+++ hx_requests/templatetags/hx_tags.py
@@ -8,37 +8,15 @@ register = template.Library()
 
 
 @register.simple_tag(takes_context=True)
-def render_hx(
-    context: Dict,
-    hx_request_name: str,
-    method: str = "get",
-    object=None,
-    use_full_path=False,
-    **kwargs,
-) -> str:
-    """
-    Renders out the hx-post or hx-get with the correct url along with all kwargs
-    turned into GET params.
-    """
-    url = get_url(context, hx_request_name, object, use_full_path, **kwargs)
-    return f"hx-{method}={url}"
-
-
-@register.simple_tag(takes_context=True)
 def hx_get(
     context: Dict, hx_request_name: str, object=None, use_full_path=False, **kwargs
 ) -> str:
     """
-    Same as render_hx EXCEPT that it is specifically for 'get' requests.
+    Renders the proper HTML attributes for an HX GET request.
+    And includes the hx_request_name, object, and any additional kwargs.
     """
-    return render_hx(
-        context,
-        hx_request_name,
-        method="get",
-        object=object,
-        use_full_path=use_full_path,
-        **kwargs,
-    )
+    url = get_url(context, hx_request_name, object, use_full_path, **kwargs)
+    return f"hx-get={url}"
 
 
 @register.simple_tag(takes_context=True)
@@ -46,16 +24,12 @@ def hx_post(
     context: Dict, hx_request_name: str, object=None, use_full_path=False, **kwargs
 ) -> str:
     """
-    Same as render_hx EXCEPT that it is specifically for 'post' requests.
+    Renders the proper HTML attributes for an HX POST request.
+    And includes the hx_request_name, object, and any additional kwargs.
+    Also includes the CSRF token if available.
     """
-    hx_attrs = render_hx(
-        context,
-        hx_request_name,
-        method="post",
-        object=object,
-        use_full_path=use_full_path,
-        **kwargs,
-    )
+    url = get_url(context, hx_request_name, object, use_full_path, **kwargs)
+    hx_attrs = f"hx-post={url}"
     token = get_csrf_token(context)
     if token:
         hx_attrs += f' hx-headers={{"X-CSRFTOKEN":"{token}"}}'
